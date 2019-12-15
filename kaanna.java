a4baa0b97ec2da25372f1fcdf6ea636a9d52d302
TwinEngineFI <52823671+TwinEngineFI@users.noreply.github.com>
2019-12-15 20:19:37 +0200

    Update kaanna.java

--- kaanna.java
+++ kaanna.java
@@ -1,22 +1,33 @@
-public class kaanna{
+public class kaannaTeksti{
   public static void main(String[] args){
     System.out.println(kaanna("merkkijono", 3, 7));
-}
+  }
 
 public static String kaanna(String s, int alku, int loppu){
   String reverse = "";
   String uusi = s.substring(alku, loppu);
+  String uusiAlku = s.substring(0,alku);
+  String uusiLoppu = "";
+  
   for(int i=uusi.length()-1; i>=0; i--){
     reverse = reverse + uusi.charAt(i);
   }
-  return reverse;
+  for(int y=uusi.length()-1; y>=loppu; y--){
+    uusiLoppu = uusiLoppu + s.charAt(y);
+    System.out.println(uusiLoppu);
+    return uusiLoppu;
+  }
+  String kokonainen = uusiAlku + reverse;
+  kokonainen = kokonainen + uusiLoppu;
+  
+  return kokonainen;
   }
 }
 
 /*
-  Kirjoita metodi kaanna(String s, int alku, int loppu), joka k‰‰nt‰‰ 
-  parametrimerkkijonon merkit p‰invaistaiseen j‰rjestykseen v‰lilt‰ 
+  Kirjoita metodi kaanna(String s, int alku, int loppu), joka k√§√§nt√§√§ 
+  parametrimerkkijonon merkit p√§invaistaiseen j√§rjestykseen v√§lilt√§ 
   [alku, loppu) ja palauttaa muutetun merkkijonon kutsujalle.
   Esimerkki:
-  System.out.println(kaanna("merkkijono", 3, 7)); // Tulostaa merkkijono 
+  System.out.println(kaanna("merkkijono", 3, 7)); // Tulostaa merjikkono  
   */
