b8ab33ba77f2b2f6c594ff8053bdd05ee4b567b6
TwinEngineFI <52823671+TwinEngineFI@users.noreply.github.com>
2019-12-15 20:27:24 +0200

    Update kaanna.java

--- kaanna.java
+++ kaanna.java
@@ -7,19 +7,13 @@ public static String kaanna(String s, int alku, int loppu){
   String reverse = "";
   String uusi = s.substring(alku, loppu);
   String uusiAlku = s.substring(0,alku);
-  String uusiLoppu = "";
+  String uusiLoppu = s.substring(loppu);
   
   for(int i=uusi.length()-1; i>=0; i--){
     reverse = reverse + uusi.charAt(i);
   }
-  for(int y=uusi.length()-1; y>=loppu; y--){
-    uusiLoppu = uusiLoppu + s.charAt(y);
-    System.out.println(uusiLoppu);
-    return uusiLoppu;
-  }
-  String kokonainen = uusiAlku + reverse;
-  kokonainen = kokonainen + uusiLoppu;
-  
+
+  String kokonainen = uusiAlku + reverse + uusiLoppu;
   return kokonainen;
   }
 }
